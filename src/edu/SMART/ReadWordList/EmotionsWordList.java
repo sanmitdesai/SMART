0f9555d375dcadebcfd2e72971b4c68f155e930b
sanmitdesai <sanmit@uga.edu>
2014-09-13 21:50:40 -0400

    2014-09-13 21:50:14.501
    WordList complete

--- src/edu/SMART/ReadWordList/EmotionsWordList.java
+++ src/edu/SMART/ReadWordList/EmotionsWordList.java
@@ -13,24 +13,34 @@ import java.util.Map;
 import edu.stanford.nlp.util.HashableCoreMap;
 
 public class EmotionsWordList {
-
-	public ArrayList<String> initializeArrayList(ArrayList<String> input){
-		for(int i=0;i<10;i++){
-			input.add("");
+	
+/********************************
+ * Initializes an ArrayList of any size with all zero's
+ * @param input
+ * @return
+ */
+	public ArrayList<String> initializeArrayList(ArrayList<String> input, int size){
+		for(int i=0;i<size-1;i++){
+			input.add("0");
 		}
 		return input;
 	}
 
-	public ArrayList<String> organizeList(ArrayList<String> input){
+	/**************************************
+	 * Creates ArrayList for emotions word list
+	 * @param input
+	 * @return
+	 */
+	public ArrayList<String> makeArrayListEmotions(ArrayList<String> input){
 
 		ArrayList<String> output = new ArrayList<String>();
-		output = this.initializeArrayList(output);
+		output = this.initializeArrayList(output,11);
 		String[] temp = input.get(0).split(",");
-		output.set(0, temp[0]);
+		output.set(0, temp[0].toUpperCase());
 
 		for(String line: input){
 			String[] element = line.split(",");
-			if(element[0].equals(output.get(0))){
+			if(element[0].toUpperCase().equals(output.get(0))){
 
 
 				if(element[1].equals("negative")){
@@ -86,6 +96,11 @@ public class EmotionsWordList {
 		return output;
 	}
 
+	/*****************************
+	 * Reads emotions word list and creats a HashMap
+	 * @param csvFile
+	 * @return
+	 */
 	public HashMap<String, ArrayList<String>> readEmotionWordList(String csvFile) {
 
 		//		String csvFile = "/Users/mkyong/Downloads/GeoIPCountryWhois.csv";
@@ -112,7 +127,10 @@ public class EmotionsWordList {
 					//					System.out.println(this.organizeList(lines));
 					
 //					lines = this.organizeList(lines);
-					output.put(this.organizeList(lines).get(0), this.organizeList(lines));
+					
+					ArrayList<String> tempArrayList = this.makeArrayListEmotions(lines);
+					
+					output.put(tempArrayList.get(0), tempArrayList);
 //					System.out.println(output.get(lines.get(0)));
 					lines.clear();
 
@@ -144,6 +162,11 @@ public class EmotionsWordList {
 		return output;
 	}
 
+	/**************************
+	 * print all elements for a given HashMap
+	 * @param mp
+	 */
+	
 	public static void printMap(HashMap<String, ArrayList<String>> mp) {
 		Collection<?> keys = mp.keySet();
 		for(Object key: keys){
@@ -151,7 +174,63 @@ public class EmotionsWordList {
 		    System.out.println(" Value " + mp.get(key));
 		}
 	}
-	public void run(String csvFile) {
+	
+	/*************************************
+	 * Creates ArrayList for SMART word list
+	 * @param element
+	 * @return
+	 */
+	
+	public ArrayList<String> makeArrayListSMART(String[] element){
+		ArrayList<String> output = new ArrayList<String>();
+		
+		output = this.initializeArrayList(output,11);
+		
+		output.set(0, element[0].toUpperCase());
+		output.set(1, element[1]);
+		
+		for(int i=2;i<element.length;i++){
+			if(element[i].equals("joy")){
+				output.set(2, element[1]);
+			}
+			else if(element[i].equals("trust")){
+				output.set(3, element[1]);
+			}
+			else if(element[i].equals("fear")){
+				output.set(4, element[1]);
+			}
+			else if(element[i].equals("surprise")){
+				output.set(5, element[1]);
+			}
+			else if(element[i].equals("sadness")){
+				output.set(6, element[1]);
+			}
+			else if(element[i].equals("disgust")){
+				output.set(7, element[1]);
+			}
+			else if(element[i].equals("anger")){
+				output.set(8, element[1]);
+			}
+			else if(element[i].equals("anticipation")){
+				output.set(9, element[1]);
+			}
+			else if(element[i].equals("none")){
+				
+			}
+			else{
+				System.err.println("In SMART Unknow emotion "+ element[i]+" for word "+element[0]);
+			}
+		}
+		
+		return output;
+	}
+	
+	/**************************************
+	 * Reads SMART word list and create HashMap for the same
+	 * @param csvFile
+	 */
+	
+	public HashMap<String, ArrayList<String>> readSMARTWordList(String csvFile, HashMap<String, ArrayList<String>> output) {
 		 
 //		String csvFile = "/Users/mkyong/Downloads/GeoIPCountryWhois.csv";
 		BufferedReader br = null;
@@ -164,8 +243,12 @@ public class EmotionsWordList {
 			while ((line = br.readLine()) != null) {
 	 
 			        // use comma as separator
-//				String[] country = line.split(cvsSplitBy);
-				System.out.println(line);
+				String[] element = line.split(cvsSplitBy);
+				
+//				System.out.println(this.makeArrayListSMART(element));
+				ArrayList<String> tempArrayList = this.makeArrayListSMART(element);
+				
+				output.put(tempArrayList.get(0), tempArrayList);
 				
 	 
 			}
@@ -184,13 +267,32 @@ public class EmotionsWordList {
 			}
 		}
 	 
-//		System.out.println("Done");
+return output;
 	  }
+	
+	/******************
+	 * creates combined word list for both emotions and SMART
+	 * @param emotionsFilePath
+	 * @param SMARTFilePath
+	 * @return
+	 */
+	
+	public HashMap<String, ArrayList<String>> makeWordList (String emotionsFilePath, String SMARTFilePath){
+		HashMap<String, ArrayList<String>> output = new HashMap<String, ArrayList<String>>();
+		
+		output=this.readEmotionWordList(emotionsFilePath);
+		
+		output = this.readSMARTWordList(SMARTFilePath, output);
+		
+		return output;
+	}
 	
 	public static void main(String[] args) {
 		EmotionsWordList objEmotionsWordList = new EmotionsWordList();
 //		printMap(objEmotionsWordList.readEmotionWordList("wordlist/emotions.csv"));
-		objEmotionsWordList.run("wordlist\\MergeLists v2.csv");
+//		objEmotionsWordList.readSMARTWordList("wordlist\\MergeLists v2.csv");
+		
+		System.out.println(objEmotionsWordList.makeWordList("wordlist/emotions.csv", "wordlist\\MergeLists v2.csv"));
 		
 	}
 }
