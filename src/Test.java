b0f39356b1bd61ab577f2f474f5c6dafe577ce72
sanmitdesai <sanmit@uga.edu>
2014-09-23 00:08:48 -0400

    2014-09-23 00:08:33.726
    extrapolator in place

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,48 @@ import org.w3c.dom.Text;
 
 
 public class Test {
+	public static boolean isBetween(double x, double lower, double upper) {
+		  return lower < x && x <= upper;
+		}
+	
+	public static int extrapolateToFiveScale(double input){
+		
+		if(isBetween(input, 0.0, 0.2)){
+			return 1;
+		}
+		else if(isBetween(input, 0.2, 0.4)) {
+			return 2;
+		}
+		else if(isBetween(input, 0.4, 0.6)) {
+			return 3;
+		}
+		else if(isBetween(input, 0.6, 0.8)) {
+			return 4;
+		}
+		else if(isBetween(input, 0.8, 1.0)) {
+			return 5;
+		}
+		else if(isBetween(input, -0.2, 0.0)) {
+			return -1;
+		}
+		else if(isBetween(input, -0.4, -0.2)) {
+			return -2;
+		}
+		else if(isBetween(input, -0.6, -0.4)) {
+			return -3;
+		}
+		else if(isBetween(input, -0.8, -0.6)) {
+			return -4;
+		}
+		else if(isBetween(input, -1.1, -0.8)) {
+			return -5;
+		}
+		else {
+			return 0;
+		}
+		
+	}
+	
 public static void main(String[] args) throws UnsupportedEncodingException {
 //	String example = "RT @richardbranson: So sad to see violence ruling over democracy in #Ukraine &amp; #Venezuela. We all need to stand up for true democracy http:…'";
 //	System.out.println(example.replaceAll("RT @.*: ", ""));
@@ -20,7 +62,9 @@ public static void main(String[] args) throws UnsupportedEncodingException {
 	
 	String data = "President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance.";
 	String temp = URLUtilities.encode(data, "UTF-8");
-	System.out.println(temp.replace("+", "%20"));
+//	System.out.println(temp.replace("+", "%20"));
+	
+	System.out.println(extrapolateToFiveScale(-0.766666219));
 	
 }
 }
--- src/edu/SMART/sentimentAnalyzer/EmotionAnalyzer.java
+++ src/edu/SMART/sentimentAnalyzer/EmotionAnalyzer.java
@@ -29,12 +29,18 @@ public class EmotionAnalyzer {
 	public static HashMap<String, ArrayList<String>> emotionsWordList = objEmotionsWordList.makeWordList("wordlist/emotions.csv", "wordlist\\emotions2.csv");
 	public static PrintAndInitialize objAndInitialize = new PrintAndInitialize();
 
-
+/**************************
+ * 
+ */
 	private EmotionAnalyzer(){
-		//		MaxentTagger tagger = new MaxentTagger("lib\\models\\english-caseless-left3words-distsim.tagger");
+		
 	}
 
-	public void analysis(String tweet){
+	/**********************
+	 * tags the sentence for POS tags and uses the same for sentiment analysis
+	 * @param tweet
+	 */
+	public ArrayList<String> analysis(String tweet){
 		//		List<List<HasWord>> sentences;
 
 		//		InputStream is = new ByteArrayInputStream(tweet.getBytes());
@@ -43,19 +49,81 @@ public class EmotionAnalyzer {
 		DocumentPreprocessor sentences = new DocumentPreprocessor(new StringReader(tweet));
 
 		//					System.out.println(sentences);
-
+		ArrayList<ArrayList<String>> temp;
+		ArrayList<String> output = null;
 		for (List<HasWord> sentence : sentences) {
 			List<TaggedWord> tSentence = tagger.tagSentence(sentence);
 
-			ArrayList<ArrayList<String>> temp = this.wordMatch(tSentence);
+			temp = this.wordMatch(tSentence);
 			objAndInitialize.displayNestedArraylist(temp);
-			System.out.println(this.combineEmotions(temp));
+			output = this.combineEmotions(temp);
 
 		}//for
+		
+		return output;
 	}
-
+/************************
+ * returns true if x is greater than the lower bound and less than or equal to the upper bound
+ * else returns false	
+ * @param x
+ * @param lower
+ * @param upper
+ * @return
+ */
+	public static boolean isBetween(double x, double lower, double upper) {
+		  return lower < x && x <= upper;
+		}
+/******************
+ * extrapolates sentiment score on the scale of 1 to -1
+ * to the scale of 5 to -5	
+ * @param input
+ * @return
+ */
+	public static int extrapolateToFiveScale(double input){
+		
+		if(isBetween(input, 0.0, 0.2)){
+			return 1;
+		}
+		else if(isBetween(input, 0.2, 0.4)) {
+			return 2;
+		}
+		else if(isBetween(input, 0.4, 0.6)) {
+			return 3;
+		}
+		else if(isBetween(input, 0.6, 0.8)) {
+			return 4;
+		}
+		else if(isBetween(input, 0.8, 1.0)) {
+			return 5;
+		}
+		else if(isBetween(input, -0.2, 0.0)) {
+			return -1;
+		}
+		else if(isBetween(input, -0.4, -0.2)) {
+			return -2;
+		}
+		else if(isBetween(input, -0.6, -0.4)) {
+			return -3;
+		}
+		else if(isBetween(input, -0.8, -0.6)) {
+			return -4;
+		}
+		else if(isBetween(input, -1.1, -0.8)) {
+			return -5;
+		}
+		else {
+			return 0;
+		}
+		
+	}
+/****************
+ * checks for a given word in sentiwordnet and returns the sentiment score 	
+ * @param input
+ * @param sentiScore
+ * @return
+ */
 	public static ArrayList<String> setWithSentiScore(ArrayList<String> input, Double sentiScore){
-		Double temp=0.0;
+		int temp=0;
 		ArrayList<String> output = new ArrayList<String>();
 		int size = input.size();
 		output.add(input.get(0));
@@ -64,12 +132,12 @@ public class EmotionAnalyzer {
 			element = input.get(i);
 			if(!element.equals(""+0)){
 				if(element.contains("-")){
-					temp = Double.parseDouble(element)*sentiScore;
+					temp =Integer.parseInt(element)*extrapolateToFiveScale(sentiScore);
 //					System.out.print(input.get(i)+" ");
 					output.add("-"+temp);
 				}
 				else{
-					temp = Double.parseDouble(element)*sentiScore;
+					temp = Integer.parseInt(element)*extrapolateToFiveScale(sentiScore);
 //					System.out.print(input.get(i)+" ");
 					output.add(""+Math.abs(temp));
 				}
@@ -83,7 +151,11 @@ public class EmotionAnalyzer {
 		output.add(input.get(size-1));
 		return output;
 	}
-
+/***************************
+ * matches each word of a sentence to express emotion for each word
+ * @param tSentence
+ * @return
+ */
 	public ArrayList<ArrayList<String>> wordMatch(List<TaggedWord> tSentence){
 		String currentWord,currentTag;
 		ArrayList<ArrayList<String>> sentence = new ArrayList<ArrayList<String>>();
@@ -123,14 +195,18 @@ public class EmotionAnalyzer {
 		//		System.out.println(sentence);
 		return sentence;
 	}
-
+/*************
+ * Combines emotions in a given sentence to express total emotions
+ * @param sentence
+ * @return
+ */
 	public ArrayList<String> combineEmotions(ArrayList<ArrayList<String>> sentence){
 		ArrayList<String> output = new ArrayList<String>();
 		output = objAndInitialize.initializeArrayList(output, 10);
-		Double tempScore;
+		int tempScore;
 		for(ArrayList<String> word : sentence){
 			for(int i=0;i<9;i++){
-				tempScore = Double.parseDouble(word.get(i+1))+Double.parseDouble(output.get(i));
+				tempScore = Integer.parseInt(word.get(i+1))+Integer.parseInt(output.get(i));
 				output.set(i,""+tempScore);
 			}
 
@@ -139,13 +215,17 @@ public class EmotionAnalyzer {
 
 		return output;
 	}
-
+/****************
+ * 
+ * @param args
+ * @throws IOException
+ */
 	public static void main(String[] args) throws IOException {
 
 		EmotionAnalyzer obj = new EmotionAnalyzer();
 
 
-		obj.analysis("Obama appalling the senate decision");
+		System.out.println(obj.analysis("Obama appalling the senate decision"));
 		//		System.out.println(emotionsWordList);
 
 	}
